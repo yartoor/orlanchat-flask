2fdb7392897b9db6224e45c07986e871c29ff0db
Артур Сабиров <ap.sabirov@bars-open.ru>
2021-08-30 02:16:08 +0300

    Добавил отправку по Enter и очистку поля ввода

--- js/chat.js
+++ js/chat.js
@@ -13,6 +13,15 @@ channel_data = {
 	'date': 'дд.мм.гггг / чч:мм'
 }
 
+document.getElementById('send-message').addEventListener('submit', function(event){
+
+    var message = document.querySelector('#chat-message-input').value;
+    channel_data['message']=message;
+    socket.emit("channel_message", channel_data);
+    document.querySelector('#chat-message-input').value='';
+
+}, false);
+
 
 socket.on('enter',function(){
     socket.emit('enter', channels_data)
@@ -29,17 +38,25 @@ document.querySelector('#leave').onclick = function(event) {
     socket.emit('leave', channels_data)
 };
 
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+    if (e.keyCode === 13) {  // enter, return
+        document.querySelector('#chat-message-submit').click();
+    }
+};
 
 document.querySelector('#chat-message-submit').onclick = function(event){
 
     var message = document.querySelector('#chat-message-input').value;
     channel_data['message']=message;
-    socket.emit("channel_message", channel_data)
+    socket.emit("channel_message", channel_data);
+    document.querySelector('#chat-message-input').value='';
 }
+
+
 socket.on('response', function(msg, cb) {
 
     document.querySelector('#chat-log').value += (msg.data.message + '\n');
                 if (cb)
                     cb();
-            });
+});
 
